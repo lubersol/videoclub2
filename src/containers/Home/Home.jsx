d50896da39905f491bdd44c2163800452c09f492
Lucia <lubersol@gmail.com>
2020-12-01 14:31:46 +0100

    añadido estilos a pagina home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -9,19 +9,24 @@ import './Home.css';
 const Home = () => {
     return (
         <div className='home'>
-            <div className='header'>
-                <div className='buttons'>
+            <header className='header'>
+                <nav className='buttons'>
                     <Link to='/rentmovie'>Populares</Link>
                     <Link to='/register'>Register</Link>
                     <Link to='/login'>Login</Link>
-                </div>
-            </div>
-            <div className='text'>Bienvenido a mi videoclub</div>
-            <div className='collage'></div>
-            <div className='footer'>
+                </nav>
+            </header>
+            <article className='title'>Bienvenido a mi videoclub</article>
+            <section className="contenedorCentral">
+                <section className='collage'></section>
+                <section className="text"><span>Suscríbete y podrás ver una gran variedad de películas y series actuales. Puedes verlas en streaming o descargarlas! <br /><br />
+                Para inscribirte sólo necesitas un correo electrónico y una contraseña y tendrás acceso a estrenos, películas  y series populares, con su ficha detallada.</span>
+                </section>
+            </section>
+            <footer className='footer'>
                 <div>videoclub App</div>
                 <div>©Lucía Bermejo</div>
-            </div>
+            </footer>
         </div>
     )
 }
