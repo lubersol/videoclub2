5c5b2668983310b5c615ece75e1bd37729f2e67f
Lucia <lubersol@gmail.com>
2020-12-15 20:40:18 +0100

    problema con CORS

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -10,57 +10,52 @@ const Register = () => {
 
     const history = useHistory();
 
-    const handleSubmit = event => {
-
-        event.preventDefault();
-        const body = {
-            name: event.target.name.value,
-            email: event.target.email.value,
-            password: event.target.password.value
-        };
-        console.log(body);
-        axios.post('https://heroku-mongo-mi-atlas.herokuapp.com/api/user', body)
-            .then(res => {
-                console.log(res.data);
-                notification.success({ message: 'Registrado!', description: 'Bienvenido! Te has registrado correctamente' })
-
-                setTimeout(() => {
-                history.push("/login")
-                }, 1000);  
-            })
-            .catch(error => 
-                { throw (error) })
-
+    const handleSubmit = async event => {
+        console.log("registrado");
+        try {
+            event.preventDefault();
+            const body = {
+                name: event.target.name.value,
+                email: event.target.email.value,
+                password: event.target.password.value
+            };
+            console.log(body);
+            await axios.post('https://heroku-mongo-mi-atlas.herokuapp.com/api/user', body)
+            notification.success({ message: 'Registrado!', description: 'Bienvenido! Te has registrado correctamente' })
+            history.push('/login')
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    return (
-        <div className="containerForm">
-            <header className="cabeceraBoton">
-                <div className='botonCaja'>
-                    <Link to='/' className='boton_atras'>Volver atras</Link>
-                </div>
+return (
+    <div className="containerForm">
+        <header className="cabeceraBoton">
+            <div className='botonCaja'>
+                <Link to='/' className='boton_atras'>Volver atras</Link>
+            </div>
+        </header>
+        <form className="formulario" onSubmit={handleSubmit}>
+            <header className="cajaTitulo">
+                <p className="titular">Registrarme</p>
             </header>
-            <form className="formulario" onSubmit={handleSubmit}>
-                <header className="cajaTitulo">
-                    <p className="titular">Registrarme</p>
-                </header>
-                <div className="campo">
-                    <input className="datos" type="text" name="name" placeholder="Introduce tu nombre" required />
-                </div>
-                <div className="campo">
-                    <input className="datos" type="email" name="email" placeholder="Introduce tu email" required />
-                </div>
-                <div className="campo">
-                    <input className="datos" type="password" name="password" placeholder="Introduce una contraseña" required />
-                </div>
-                <div className="campo">
-                    <button type="submit" className="enviar">
-                        Enviar
+            <div className="campo">
+                <input className="datos" type="text" name="name" placeholder="Introduce tu nombre" required />
+            </div>
+            <div className="campo">
+                <input className="datos" type="email" name="email" placeholder="Introduce tu email" required />
+            </div>
+            <div className="campo">
+                <input className="datos" type="password" name="password" placeholder="Introduce una contraseña" required />
+            </div>
+            <div className="campo">
+                <button type="submit" className="enviar">
+                    Enviar
                                 </button>
-                </div>
-            </form>
-        </div>
-    );
+            </div>
+        </form>
+    </div>
+);
 };
 
 export default Register;
